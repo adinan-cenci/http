f1d192365a8244318f189b7fbb7724fff19e4dd8
Adinan Cenci <adinancenci@protonmail.com>
2026-05-25 22:28:02 +0100

    2.0.1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./src/Http.js');
+import Http from './src/Http';
+import HelperUrl from './src/HelperUrl';
+import HelperString from './src/HelperString';
+import Convert from './src/Convert';
+import CastDown from './src/CastDown';
+
+export default Http;
+export { HelperUrl, HelperString, Convert, CastDown };
